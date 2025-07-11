01a67e8f5f53e82ec46f48d31a19ae3e19f3c9df
fansa543 <alffadilah76@gmail.com>
2025-07-11 11:57:20 +0700

    Tambah Constructor

--- src/UAS/Manhua.java
+++ src/UAS/Manhua.java
@@ -3,3 +3,14 @@ package UAS;
 public class Manhua {
     private String judul, penulis, genre;
 
+    public Manhua(String judul, String penulis, String genre) {
+        this.judul = judul;
+        this.penulis = penulis;
+        this.genre = genre;
+    }
+
+
+
+    
+
+
