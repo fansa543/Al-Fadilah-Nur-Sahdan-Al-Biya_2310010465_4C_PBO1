5e12163366ad6b6a26b086500c27fbe253f9e5fc
fansa543 <alffadilah76@gmail.com>
2025-07-11 11:59:11 +0700

    tambah accesor dan mutator

--- src/UAS/Manhua.java
+++ src/UAS/Manhua.java
@@ -9,6 +9,17 @@ public class Manhua {
         this.genre = genre;
     }
 
+    public String getJudul() { return judul; }
+    public void setJudul(String judul) { this.judul = judul; }
+
+    public String getPenulis() { return penulis; }
+    public void setPenulis(String penulis) { this.penulis = penulis; }
+
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+
+   
+
 
 
     
