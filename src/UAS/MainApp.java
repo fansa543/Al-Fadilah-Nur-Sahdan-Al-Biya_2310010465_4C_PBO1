725e4838ee603eb2971f2a47cf7ee23f4c0e2df8
fansa543 <alffadilah76@gmail.com>
2025-07-11 12:21:13 +0700

    + array

--- src/UAS/MainApp.java
+++ src/UAS/MainApp.java
@@ -5,5 +5,8 @@ import java.util.Scanner;
 public class MainApp {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        DetailManhua[] koleksi = new DetailManhua[2];
+        
+
         
 
