9b843132fb03682a3d37e0f9c099f0185abc9a06
fansa543 <alffadilah76@gmail.com>
2025-07-11 12:21:51 +0700

    + error handling

--- src/UAS/MainApp.java
+++ src/UAS/MainApp.java
@@ -6,6 +6,8 @@ public class MainApp {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         DetailManhua[] koleksi = new DetailManhua[2];
+        try {
+           
         
 
         
